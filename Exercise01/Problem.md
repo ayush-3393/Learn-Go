0a6dab6c22d649a3d531d3db517cc5222b94af47
Ayush Sinha <ayush.sinha3393@gmail.com>
2025-05-12 18:50:24 +0530

    exercise01

---
+++ Exercise01/Problem.md
@@ -0,0 +1,2 @@
+1. Printing Text with some Emojis
+2. Printing raw string literals.
---
+++ Exercise01/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Printing Some Text With Emojis", "👀 👀 👀")
+	fmt.Println(`
+		Trying to print raw string literals
+		With some emojis
+		👀 👀 👀
+		And some more text
+		Only to see if it works
+	`)
+}
